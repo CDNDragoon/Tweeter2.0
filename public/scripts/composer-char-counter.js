81f1d99d2c71be9fb3a7222e5513c94cf1f9f808
CDNDragoon <tjgossage@gmail.com>
2021-06-15 19:24:02 +0000

    counter functional

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("textarea").keyup(function() {
+    let counter = 140 - ($(this).val().length) 
+    if (counter >= 0) {
+      $("output").replaceWith(`<output name="counter" class="counter" for="tweettext">${counter}</output>`)
+    } else {
+      $("output").replaceWith(`<output style="color: red" name="counter" class="counter" for="tweettext">${counter}</output>`)
+    }
+    console.log(counter)
+  })
+});
